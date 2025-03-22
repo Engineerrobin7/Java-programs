48600e3850f521f36607ccb34c264e3346844d24
Robin <santoshjitu1980@gmail.com>
2025-03-22 14:11:53 +0530

    Conditionals

--- com/robin/largestnumber.java
+++ com/robin/largestnumber.java
@@ -8,9 +8,9 @@ public class largestnumber {
         try (Scanner in = new Scanner(System.in)) {
             System.out.print("Enter the first number: ");
             int a = in.nextInt();
-            System.out.print("Enter the second number: ");
+            System.out.println("Enter the second number: ");
             int b = in.nextInt();
-            System.out.print("Enter the third number: ");
+            System.out.println("Enter the third number: ");
             int c = in.nextInt();
 
             max = a;
