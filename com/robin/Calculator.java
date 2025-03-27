6390ad04fdda9fd0cd7d24dd44e9cd29a88b3857
Robin <santoshjitu1980@gmail.com>
2025-03-27 20:39:00 +0530

    Refactor Calculator program to improve operator handling and add exit option; create new switchstatement class

--- com/robin/Calculator.java
+++ com/robin/Calculator.java
@@ -1,21 +1,47 @@
 package com.robin;
+
 import java.util.Scanner;
+
 public class Calculator {
-    Scanner in = new Scanner (System.in);
-    int ans= 0;
-    while(true){
-        System.out.println("Enter the operator:");
-        char op = in.next().trim().charAt(0);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
 
-        if(op =='+'||op =='-'||op =='*'||op=='/'||op=='%'){
-            System.out.println("Enter two number:");
-            int num1=in.nextInt();
-            int num2=in.nextInt();
+        while (true) {
+            System.out.println("Enter the operator (+, -, *, /, %, x to exit):");
+            char op = in.next().trim().charAt(0);
 
-            if (op=='+'){
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.println("Enter two numbers:");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
 
+                if (op == '+') {
+                    ans = num1 + num2;
+                } else if (op == '-') {
+                    ans = num1 - num2;
+                } else if (op == '*') {
+                    ans = num1 * num2;
+                } else if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    } else {
+                        System.out.println("Division by zero is not allowed!");
+                        continue;
+                    }
+                } else if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid Operator!!");
+                continue;
             }
+
+            System.out.println("The result is: " + ans);
         }
-    } 
-    
+
+        in.close();
+    }
 }
