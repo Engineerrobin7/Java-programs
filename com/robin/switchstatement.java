86ad287c1a9734b4284493eb4eb844ac1cf04eca
Robin <santoshjitu1980@gmail.com>
2025-06-02 09:02:52 +0530

    Fruit fix errors

--- com/robin/switchstatement.java
+++ com/robin/switchstatement.java
@@ -1,24 +1,26 @@
 package com.robin;
 import java.util.Scanner;
+
 public class switchstatement {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Enter a fruit name:");
         String fruit = in.next();
 
-        switch(fruit){
+        switch (fruit) {
             case "Mango":
                 System.out.println("King of Fruits");
                 break;
             case "Apple":
-                System.out.println("A sweet red fruit"); 
+                System.out.println("A sweet red fruit");
                 break;
             case "Orange":
-                System.out.println("Sweety and Bitter ");  
+                System.out.println("Sweety and Bitter");
                 break;
             default:
-                System.out.println("Please Enter a valid fruit");         
+                System.out.println("Please Enter a valid fruit");
         }
 
+        in.close();
     }
-    
 }
