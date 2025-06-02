65f22a238c3644f50af9109c8fb3ef246823d83b
Your Name <your_email@example.com>
2025-06-02 08:43:26 +0530

    passing example

---
+++ com/robin/passingeg.java
@@ -0,0 +1,12 @@
+package com.robin;
+public class passingeg {
+    public static void main(String[] args){
+        String name="Robin Singh";
+        greet(name);
+    }
+    static void main(String[] naam){
+        System.out.println(naam);
+
+    }
+    
+}
